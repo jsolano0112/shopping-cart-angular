972299b14ad2db1462de3bf22f90ce10810c9815
jsolano0112 <juana.solano@correo.tdea.edu.co>
2023-12-14 18:50:18 -0500

    Add filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -17,13 +18,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FilterComponent } from './pages/home/components/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ProductsHeaderComponent
+    ProductsHeaderComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +34,7 @@ import { ProductsHeaderComponent } from './pages/home/components/products-header
     BrowserAnimationsModule,
     MatSidenavModule,
     MatGridListModule,
+    MatCardModule,
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ProductsHeaderComponent {
+export class ProductsHeaderComponent implements OnInit {
+@Output() columnsCountChange = new EventEmitter<number>();
+ sort = 'desc';
+ itemsShowCount = 12;
 
+ constructor(){
+
+ }
+
+ ngOnInit(): void {
+   
+ }
+
+ onSortUpdated(newSort: string): void {
+  this.sort = newSort;
+
+ }
+
+ onItemsUpdated(count: number): void{
+  this.itemsShowCount = count;
+ }
+
+ onColumnsUpdated(colsNum: number): void{
+  this.columnsCountChange.emit(colsNum);
+ }
 }
