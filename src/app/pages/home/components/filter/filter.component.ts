7b86b2e11171da2853e732a9859b8e867d88d421
jsolano0112 <juana.solano@correo.tdea.edu.co>
2023-12-16 15:02:05 -0500

    Modify filter

--- src/app/pages/home/components/filter/filter.component.ts
+++ src/app/pages/home/components/filter/filter.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html'
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
+  @Output() showCategory = new EventEmitter<string>();
+
   categories = ['shoes', 'sport'];
+
+  ngOnInit(): void {
+
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,11 +6,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  category: string | undefined;
+
   ngOnInit(): void {
 
   }
 
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 }
