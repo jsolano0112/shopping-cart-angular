bf8ccf6c63367b06979c937b451a9d907522ba83
jsolano0112 <juana.solano@correo.tdea.edu.co>
2023-12-17 14:18:37 -0500

    Modify pages and add cart

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -8,10 +9,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProductBoxComponent implements OnInit {
   @Input() fullWidthMode = false;
+  @Output() addToCard = new EventEmitter();
+
+  product: Product | undefined = {
+    id: 1,
+    title: 'Snickers',
+    price:150,
+    category: 'shoes',
+    description: 'Description',
+    image: 'https://via.placeholder.com/150'
+  };
   constructor(){}
 
   ngOnInit(): void {
     
   }
 
+  onAddToCart(): void{
+    this.addToCard.emit(this.product);
+  }
 }
