5cad19d3d6968ccd7ff5fb5bf7f0780b9efeb231
jsolano0112 <juana.solano@correo.tdea.edu.co>
2023-12-18 22:51:48 -0500

    Add cart services

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
@@ -9,6 +11,10 @@ export class HomeComponent implements OnInit {
   cols = 3;
   rowHeight =  ROWS_HEIGHT[this.cols];
   category: string | undefined;
+
+  constructor(private cartService: CartService){
+
+  }
 
   ngOnInit(): void {
 
@@ -21,5 +27,15 @@ export class HomeComponent implements OnInit {
 
   onShowCategory(newCategory: string): void {
     this.category = newCategory;
+  }
+
+  onAddToCart(product: Product): void{
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    })
   }
 }
