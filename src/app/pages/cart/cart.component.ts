fb6b9e2d4191812383664355c74d26e77111ddfe
jsolano0112 <juana.solano@correo.tdea.edu.co>
2023-12-19 22:03:49 -0500

    Add getTotal and clearCart methods

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -35,11 +36,12 @@ export class CartComponent implements OnInit {
     'action'
   ]
 
+  constructor(private cartService: CartService){}
   ngOnInit(): void {
     this.dataSource = this.cart.items;
   }
 
   getTotal(items: Array<CartItem>): number {
-    return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0);
+    return this.cartService.getTotal(items);
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -27,4 +27,14 @@ export class CartService {
 
   }
 
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0);
+  }
+
+  clearCart(): void{
+    this.cart.next({items: []});
+    this._snackBar.open('Cart is cleared.', 'Ok',{
+      duration: 3000
+    });
+  }
 }
